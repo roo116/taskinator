8e5557203d2a1adba250932bc461c262a6752b90
roo <roo116@nc.rr.com>
2022-05-03 17:27:41 -0400

    completed event handler and form function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonE1 = document.querySelector("#save-task");
-var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
+var createTaskHandler = function(event) {
+
+  event.preventDefault();
+
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonE1.addEventListener("click", createTaskHandler); 
+formEl.addEventListener("submit", createTaskHandler);
